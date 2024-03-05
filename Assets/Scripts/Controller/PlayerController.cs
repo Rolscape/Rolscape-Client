559ae56335f21f62b8756ca4aea24b4216cd5ba8
jonghyeok98 <dlwhdgur113@naver.com>
2024-03-05 22:10:22 +0900

    Add InputManager ResourceManager

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -2,37 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private float _speed = 10.0f;
     private CharacterController _characterController;
     void Start()
     {
-        
         if (_characterController == null)
         {
             gameObject.AddComponent<CharacterController>();
         }
-        _characterController = GetComponent<CharacterController>();            
-        
+        _characterController = GetComponent<CharacterController>();
 
+        Managers.Input.KeyAction -= OnKeyboard;
+        Managers.Input.KeyAction += OnKeyboard;
 
     }
 
     void Update()
+    {
+        
+         
+    }
+
+    void OnKeyboard()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
         Vector3 dir = new Vector3(h, 0, v).normalized;
         
-        // TODO CharcterController로 움직이기  
 
         if (dir != Vector3.zero)
         {
             _characterController.Move(dir*(_speed*Time.deltaTime));
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 0.3f);   
         }
-         
     }
 }
--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class Managers : MonoBehaviour
 {
     private static Managers s_instance;
-    public static Managers Instance
+    private static Managers Instance
     {
         get
         {
             Init();
             return s_instance;
         }
+    }
+
+    private InputManager _input = new InputManager();
+    private ResourceManager _resource = new ResourceManager();
+    public static InputManager Input
+    {
+        get { return Instance._input; }
+    }
+    public static ResourceManager Resource
+    {
+        get { return Instance._resource; }
     }
     
     void Start()
@@ -21,7 +32,7 @@ public class Managers : MonoBehaviour
 
     void Update()
     {
-        
+        _input.OnUpdate();          // 리스너 패턴을 이용해 입력을 받아옴
     }
 
     static void Init()
