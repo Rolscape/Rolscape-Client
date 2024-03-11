83fc80916137f0017a0455a0ce8ec8a51edcfcc8
jonghyeok98 <dlwhdgur113@naver.com>
2024-03-11 22:28:15 +0900

    Add SoundManager

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -13,6 +13,7 @@ public class Managers : MonoBehaviour
     private ResourceManager _resource = new ResourceManager();
     private SceneManagerEx _scene = new SceneManagerEx();
     private UIManager _ui = new UIManager();
+    private SoundManager _sound = new SoundManager();
     
     // Network Manager
     private NetworkManager networkManager = new NetworkManager();
@@ -31,6 +32,8 @@ public class Managers : MonoBehaviour
     public static ResourceManager Resource { get { return Instance._resource; } }
     public static UIManager UI { get { return Instance._ui; } }
     public static SceneManagerEx Scene { get { return Instance._scene; } }
+    public static SoundManager Sound { get { return Instance._sound; }
+    }
 
     public static NetworkManager Network { get { return Instance.networkManager; } }
     public static VivoxManager Vivox { get { return Instance.vivoxManager; } }
