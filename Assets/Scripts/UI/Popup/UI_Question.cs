7434e9cf3f179dd215934cc497861bf53ad870a4
이종혁 <gamza@ijonghyeog-ui-MacBookAir.local>
2024-04-01 16:23:38 +0900

    fix

--- Assets/Scripts/UI/Popup/UI_Question.cs
+++ Assets/Scripts/UI/Popup/UI_Question.cs
@@ -62,11 +62,13 @@ public class UI_Question : UI_Popup
         inputField.onEndEdit.AddListener(OnEndEdit);
     }
 
-    void SetImage()
+    void SetImage(string path = null)
     {
         Image image = GetImage((int)Images.Question);
         image.rectTransform.sizeDelta = new Vector2(1500.0f, 800.0f);
-        Texture2D texture2D = Managers.Resource.Load<Texture2D>("Arts/Question/Question1");
+        if (string.IsNullOrEmpty(path))
+            path = "Question1";
+        Texture2D texture2D = Managers.Resource.Load<Texture2D>($"Arts/Question/{path}");
         Sprite sprite = Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), Vector2.one * 0.5f);
         image.sprite = sprite;
     }
