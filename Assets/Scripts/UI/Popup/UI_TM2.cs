5d3d5a23661ecf8de508b321e41f262c3e586082
jonghyeok98 <dlwhdgur113@naver.com>
2024-05-16 15:30:33 +0900

    Add TM2

--- Assets/Scripts/UI/Popup/UI_TM2.cs
+++ Assets/Scripts/UI/Popup/UI_TM2.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UI_TM2 : UI_Popup
 {
-    // Start is called before the first frame update
-    void Start()
+    public Action<int> putPuzzleAction = null;
+    enum Images
     {
+        Background,
+        Puzzle,
+    }
+    
+    public override void Init()
+    {
+        base.Init();
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    void BIndUI()
+    {
+        Bind<Image>(typeof(Images));
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void putPuzzle(int idx)
     {
+
         
     }
+
+    public void OnDragEnded(PointerEventData data)
+    {
+        // TODO Image 판단
+        
+        int idx = 0;
+        
+        
+        // TODO Send ID to Sever   
+    }
+
+    public void Clear()
+    {
+        putPuzzleAction = null;
+    }
+
+    
 }
