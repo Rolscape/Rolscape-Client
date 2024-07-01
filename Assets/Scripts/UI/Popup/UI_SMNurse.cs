ae2b1f10e03683dd46f53cb2c220b23ab59c7bc3
sim3108 <dev3108@naver.com>
2024-07-01 13:25:36 +0900

    [심재성] 양호실 미니게임 추가

--- Assets/Scripts/UI/Popup/UI_SMNurse.cs
+++ Assets/Scripts/UI/Popup/UI_SMNurse.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,15 +12,30 @@ public class UI_SMNurse : UI_SM
         Timer
     }
 
+    public List<Sprite> sources = new List<Sprite>();
+    public RectTransform beds;
     public Image[] answer;
+
+    public GameObject exampleView;
+    public Image[] example;
+
+    public Image dragObject;
+    
+
+    private List<int> answerList = new List<int>();
     private int answerCount;
     private bool isGameStart;
+    private bool isDrag;
+    private bool isClear;
+    private int selectIndex;
+    private int slotIndex;
 
     public override void Init()
     {
         base.Init();
         Managers.Sound.Play("MinigameSlow", Define.Sound.Bgm);
         BiundUI();
+        SetAnswer();
         StartCoroutine(AnswerView());
     }
 
@@ -28,22 +44,77 @@ public class UI_SMNurse : UI_SM
         Init();
     }
 
+    void Update()
+    {
+        if (isDrag)
+        {
+            dragObject.GetComponent<RectTransform>().position = Input.mousePosition;
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                isDrag = false;
+                dragObject.gameObject.SetActive(false);
+                slotIndex = GetSlotIndex();
+                if (slotIndex >= 0)
+                {
+                    answer[slotIndex].gameObject.SetActive(true);
+                }
+                else
+                {
+                    example[selectIndex].gameObject.SetActive(true);
+                }
+            }
+        }
+
+        if (IsClearCheck() && isGameStart && !isClear)
+        {
+            isClear = true;
+            Clear();
+        }
+    }
+
     void BiundUI()
     {
         Bind<TextMeshProUGUI>(typeof(Texts));
+
+        isDrag = false;
+        isClear = false;
+        dragObject.gameObject.SetActive(false);
+
         countTimer = 30;
         timerText = GetText((int)Texts.Timer);
+        beds.anchoredPosition = new Vector2(0f, -60f);
+        exampleView.SetActive(false);
         //timerText.text = countTimer.ToString("D2");
+    }
+
+    void SetAnswer()
+    {
+        answerList.Clear();
+
+        // 0~19Áß ·£´ý
+        int random;
+        while (true)
+        {
+            random = Random.Range(0, 19);
+            if (!answerList.Exists(match => match == random))
+            {
+                answerList.Add(random);
+
+                if (answerList.Count == 6)
+                    break;
+            }
+        }
     }
 
     IEnumerator AnswerView()
     {
         isGameStart = false;
         answerCount = 5;
-        foreach (var img in answer)
+        for (int i = 0; i < answer.Length; ++i)
         {
-            img.color = Random.ColorHSV();
-            img.gameObject.SetActive(true);
+            answer[i].sprite = sources[answerList[i]];
+            answer[i].gameObject.SetActive(true);
         }
         yield return null;
 
@@ -71,9 +142,62 @@ public class UI_SMNurse : UI_SM
         {
             img.gameObject.SetActive(false);
         }
+        beds.anchoredPosition = new Vector2(-200f, -60f);
+
+        var temp = new List<int>();
+        var list = new List<int>() { 0, 1, 2, 3, 4, 5 };
+        var random = new System.Random();
+
+        var randomized = list.OrderBy(x => random.Next());
+        foreach (var i in randomized)
+        {
+            temp.Add(i);
+        }
+
+        for (int i = 0; i < example.Length; ++i)
+        {
+            example[temp[i]].sprite = sources[answerList[i]];
+        }
+        exampleView.SetActive(true);
 
         StopCoroutine(AnswerView());
         timerText.text = countTimer.ToString("D2");
         StartCoroutine(TimerCoroutine());
+    }
+
+    int GetSlotIndex()
+    {
+        Vector3 mPos = dragObject.GetComponent<RectTransform>().position;
+
+        for (int i = 0; i < answer.Length; ++i)
+        {
+            if (Vector3.Distance(mPos, answer[i].GetComponent<RectTransform>().position) < 100 && dragObject.sprite.name.Equals(answer[i].sprite.name))
+            {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+
+    bool IsClearCheck()
+    {
+        int clearCount = 0;
+        for (int i = 0; i < answer.Length; ++i)
+        {
+            if (answer[i].gameObject.activeSelf)
+                clearCount++;
+        }
+
+        return clearCount == 6;
+    }
+
+    public void PointerDown(int _idx)
+    {
+        isDrag = true;
+        selectIndex = _idx;
+        example[_idx].gameObject.SetActive(false);
+        dragObject.gameObject.SetActive(true);
+        dragObject.sprite = example[_idx].sprite;
     }
 }
