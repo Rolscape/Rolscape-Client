1cd311a59f96d94da8c669ce5d30987f4a2a1713
sim3108 <dev3108@naver.com>
2024-07-15 11:02:27 +0900

    [심재성] 과학실 불끄기 추가

--- Assets/Scripts/UI/Popup/UI_SMScience.cs
+++ Assets/Scripts/UI/Popup/UI_SMScience.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Pool;
 using UnityEngine.UI;
 
 public class UI_SMScience : UI_SM
 {
     public List<RectTransform> positions;
+    [SerializeField] private GameObject waterPrefab;
+
+    private IObjectPool<WaterBall> waterPool;
+    private Vector3 waterStartPos = new Vector3(-853f, -287f, 0);
+    private Vector3 waterEndPos = Vector3.zero;
+    private float waterBalltTime;
+    private bool isSpaceBar;
 
     public override void Init()
     {
@@ -17,14 +25,66 @@ public class UI_SMScience : UI_SM
         SetFire();
     }
 
+    void Awake()
+    {
+        waterPool = new ObjectPool<WaterBall>(
+                PoolCreate,
+                PoolGet,
+                PoolRelease,
+                PoolDestroy,
+                maxSize: 10
+            );
+    }
+
     void Start()
     {
         Init();
+        isSpaceBar = false;
+
+        // player
+        MyPlayerController player = FindObjectOfType<MyPlayerController>();
+        if (player != null)
+            player._isMission = true;
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isSpaceBar = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isSpaceBar = false;
+        }
+
+        if (isSpaceBar)
+        {
+            if (waterBalltTime >= 0.3f)
+            {
+                waterPool.Get();
+                waterBalltTime = 0f;
+            }
+
+            waterBalltTime += Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                waterEndPos += Vector3.up;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                waterEndPos += Vector3.down;
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                waterEndPos += Vector3.left;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                waterEndPos += Vector3.right;
+            }
+        }
     }
 
     void BiundUI()
@@ -54,4 +114,65 @@ public class UI_SMScience : UI_SM
                 break;
         }
     }
+
+    public void CheckFire(Vector3 _goal)
+    {
+        for (int i = 0; i < positions.Count; ++i)
+        {
+            if (positions[i].gameObject.activeSelf)
+            {
+                if (Vector3.Distance(positions[i].localPosition, _goal) < 50)
+                {
+                    positions[i].gameObject.SetActive(false);
+                }
+            }
+        }
+
+        bool isEndGame = true;
+        foreach (var pos in positions)
+        {
+            if (pos.gameObject.activeSelf)
+            {
+                isEndGame = false;
+                break;
+            }
+        }
+
+        if (isEndGame)
+        {
+            MyPlayerController player = FindObjectOfType<MyPlayerController>();
+            if (player != null)
+                player._isMission = false;
+            
+            Managers.Sound.Play("MainBgm", Define.Sound.Bgm);
+            ClosePopupUI();
+        }
+    }
+
+    #region Pool
+    private WaterBall PoolCreate()
+    {
+        WaterBall go = Instantiate(waterPrefab.GetComponent<WaterBall>(), transform);
+        go.SetPool(waterPool);
+        return go;
+    }
+
+    private void PoolGet(WaterBall water)
+    {
+        water.transform.localPosition = waterStartPos;
+        water.gameObject.SetActive(true);
+        water.SetGoal(waterEndPos);
+    }
+
+    private void PoolRelease(WaterBall water)
+    {
+        CheckFire(water.GetGoal());
+        water.gameObject.SetActive(false);
+    }
+
+    private void PoolDestroy(WaterBall water)
+    {
+        Destroy(water.gameObject);
+    }
+    #endregion
 }
---
+++ Assets/Scripts/UI/WaterBall.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class WaterBall : MonoBehaviour
+{
+    private Vector3 goalPos;
+    private IObjectPool<WaterBall> waterPool;
+    private float moveSpeed = 5.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (waterPool == null)
+            return;
+
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, goalPos, moveSpeed);
+        //transform.localPosition = Vector3.Lerp(transform.localPosition, goalPos, Time.deltaTime * moveSpeed);
+
+        if (Vector3.Distance(transform.localPosition, goalPos) <= 0.5f)
+        {
+            waterPool.Release(this);
+        }
+    }
+
+    public void SetPool(IObjectPool<WaterBall> _pool)
+    {
+        waterPool = _pool;
+        goalPos = Vector3.zero;
+    }
+
+    public void SetGoal(Vector3 _goal)
+    {
+        goalPos = _goal;
+    }
+
+    public Vector3 GetGoal()
+    {
+        return goalPos;
+    }
+}
