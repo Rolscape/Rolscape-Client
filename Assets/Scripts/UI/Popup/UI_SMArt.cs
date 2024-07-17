dd47cda68733fd16dca8e72cefe7c8e362682396
sim3108 <dev3108@naver.com>
2024-07-17 10:20:02 +0900

    [심재성] 틀린그림찾기 기능 추가

--- Assets/Scripts/UI/Popup/UI_SMArt.cs
+++ Assets/Scripts/UI/Popup/UI_SMArt.cs
@@ -11,6 +11,14 @@ public class UI_SMArt : UI_SM
         Timer
     }
 
+    public List<GameObject> question;
+    public List<GameObject> clearImage;
+    public RectTransform imageX;
+
+    private List<GameObject> leftClear = new List<GameObject>();
+    private List<GameObject> rightClear = new List<GameObject>();
+    private int clearCount;
+
     public override void Init()
     {
         base.Init();
@@ -21,6 +29,7 @@ public class UI_SMArt : UI_SM
     void Start()
     {
         Init();
+        SetGame();
     }
 
     void Update()
@@ -36,5 +45,80 @@ public class UI_SMArt : UI_SM
         timerText = GetText((int)Texts.Timer);
         timerText.text = countTimer.ToString("D2");
         StartCoroutine(TimerCoroutine());
+    }
+
+    void SetGame()
+    {
+        foreach (var image in question)
+        {
+            image.SetActive(false);
+        }
+
+        foreach (var image in clearImage)
+        {
+            image.SetActive(false);
+        }
+
+        imageX.gameObject.SetActive(false);
+        int random = Random.Range(0, question.Count);
+        question[random].SetActive(true);
+        clearCount = 0;
+
+        GameObject left = Util.FindChild(question[random], "Left", true);
+        GameObject right = Util.FindChild(question[random], "Right", true);
+
+        for (int i = 0; i < clearImage.Count; ++i)
+        {
+            leftClear.Add(Util.FindChild(left, $"Check_{i}", true));
+            rightClear.Add(Util.FindChild(right, $"Check_{i}", true));
+        }
+
+        for (int i = 0; i < clearImage.Count; ++i)
+        {
+            leftClear[i].SetActive(false);
+            rightClear[i].SetActive(false);
+        }
+    }
+
+    void SetDot()
+    {
+        for (int i = 0; i < clearImage.Count; ++i)
+        {
+            clearImage[i].SetActive(i < clearCount);
+        }
+    }
+
+    void DisableX()
+    {
+        imageX.gameObject.SetActive(false);
+    }
+
+    void MissionClear()
+    {
+        Clear();
+    }
+
+    public void OnClick_X()
+    {
+        imageX.position = Input.mousePosition;
+        imageX.gameObject.SetActive(true);
+        Invoke("DisableX", 0.5f);
+    }
+
+    public void OnClick_O(int _idx)
+    {
+        leftClear[_idx].SetActive(true);
+        rightClear[_idx].SetActive(true);
+
+        leftClear[_idx].transform.parent.GetComponent<Button>().interactable = false;
+        rightClear[_idx].transform.parent.GetComponent<Button>().interactable = false;
+
+        clearCount++;
+        SetDot();
+
+        if (clearCount == 5)
+        {
+            Invoke("MissionClear", 0.5f);
+        }
     }
 }
